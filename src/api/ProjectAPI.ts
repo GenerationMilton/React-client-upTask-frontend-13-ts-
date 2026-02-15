4027932f52b48d95119590f305d8ae69da2ccf0d
Milton Munoz <livemilton@gmail.com>
2026-02-14 20:04:47 -0500

    frontend-application :sparkles: How to read backend errors with useMutation

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,5 +1,6 @@
 import type { ProjectFormData } from "@/types/index";
 import api from "@/lib/axios";
+import { isAxiosError } from "axios";
 
 export async function createProject(formData : ProjectFormData) {
     try {
@@ -7,6 +8,8 @@ export async function createProject(formData : ProjectFormData) {
         console.log(data)
         return data
     } catch (error) {
-        console.log(error)
+        if(isAxiosError(error) && error.response){
+            throw new Error(error.response.data.error)
+        }
     }
 }
