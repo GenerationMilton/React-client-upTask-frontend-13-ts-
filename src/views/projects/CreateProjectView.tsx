625a9933e9c4bbf88bb9bfc0954d2291f2c7be05
Milton Munoz <livemilton@gmail.com>
2026-02-14 19:48:24 -0500

    frontend-application :sparkles: Create projects with React-query mutations

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -4,6 +4,7 @@ import ProjectForm from "@/components/projects/ProjectForm";
 import type { ProjectFormData } from "@/types/index";
 import { createProject } from "@/api/ProjectAPI";
 import { toast } from "react-toastify";
+import { useMutation } from "@tanstack/react-query";
 
 export default function CreateProjectView() {
   
@@ -16,10 +17,20 @@ export default function CreateProjectView() {
 
   const { register, handleSubmit, formState: {errors}} = useForm({defaultValues: initialValues})
 
-  const handleForm = async (formData : ProjectFormData) => {
-      const data = await createProject(formData)
+  //React Query
+  const { mutate } = useMutation({
+    mutationFn: createProject,
+    onError: () =>{
+
+    },
+    onSuccess:(data) =>{
       toast.success(data)
       navigate('/')
+    }
+  })
+
+  const handleForm = (formData : ProjectFormData) => {
+      mutate(formData)
 
     }
 
