306429b16083e7c04a4d9fed5353105d15a664fb
Milton Munoz <livemilton@gmail.com>
2026-02-18 18:55:26 -0500

    frontend-application :sparkles: First details of the project view

--- src/views/projects/ProjectDetailsView.tsx
+++ src/views/projects/ProjectDetailsView.tsx
@@ -1,6 +1,41 @@
+import { Navigate, useNavigate, useParams } from "react-router-dom"
+import { useQuery } from '@tanstack/react-query'
+import { getProjectById } from "@/api/ProjectAPI"
+import EditProjectForm from "./EditProjectForm"
+import AddTaskModal from "@/components/tasks/AddTaskModal"
 
 export default function ProjectDetailsView() {
-  return (
-    <div>ProjectDetailsView</div>
-  )
+    const navigate = useNavigate()
+    const params = useParams()
+    console.log(params)
+    const projectId = params.projectId!
+    console.log(projectId)
+
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ['editProject', projectId],
+        queryFn: () => getProjectById(projectId),
+        retry: false
+    })
+    console.log(data)
+    if(isLoading) return 'Cargando...'
+    if(isError) return <Navigate to='/404' />
+    if(data) return (
+        <>
+            <h1 className="text-5xl font-black">{data.projectName}</h1>
+            <p className="text-2xl font-light text-gray-500 mt-5">{data.description}</p>
+
+            <nav className="my-5 flex gap-3">
+                <button
+                    type="button"
+                    className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl
+                    font-bold cursor-pointer transition-colors"
+                    onClick={() => navigate('?newTask=true')}
+                >Agregar Tarea
+                </button>
+            </nav>
+
+            <AddTaskModal />
+        </>
+    )
+
 }
