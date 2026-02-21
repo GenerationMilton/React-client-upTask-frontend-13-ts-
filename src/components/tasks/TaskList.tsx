c5b95c94bd0ede869f331a5b7a8cc01f8e44e8c3
Milton Munoz <livemilton@gmail.com>
2026-02-21 05:04:35 -0500

    frontend-application :sparkles: Applying different colors to the states

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -27,6 +27,15 @@ const statusTranslations : {[key: string]: string} = {
     completed: 'Completado',
 }
 
+const statusStyles : {[key: string]: string} = {
+    pending: 'border-t-slate-500',
+    onHold: 'border-t-red-500',
+    inProgress: 'border-t-blue-500',
+    underReview: 'border-t-amber-500',
+    completed: 'border-t-emerald-500',
+}
+
+
 export default function TaskList({tasks} : TaskListProps) {
         const groupedTasks = tasks.reduce((acc, task) => {
         let currentGroup = acc[task.status] ? [...acc[task.status]] : [];
@@ -42,7 +51,10 @@ export default function TaskList({tasks} : TaskListProps) {
             <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
                 {Object.entries(groupedTasks).map(([status, tasks]) => (
                     <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
-                        <h3 className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8`}>{statusTranslations[status]}</h3>
+                        <h3 className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8
+                        ${statusStyles[status]}`}
+                        
+                        >{statusTranslations[status]}</h3>
 
                         <ul className='mt-5 space-y-5'>
                             {tasks.length === 0 ? (
