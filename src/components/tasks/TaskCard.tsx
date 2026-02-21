0a2379843877921c9cccfed4d6516378492ca7be
Milton Munoz <livemilton@gmail.com>
2026-02-21 05:22:48 -0500

    frontend-application :sparkles: Getting started with task editing

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -1,13 +1,17 @@
 import type { Task } from "@/types/index"
-import { Menu, MenuItem, MenuItems, Transition } from "@headlessui/react"
+import { Menu, MenuButton, MenuItem, MenuItems, Transition } from "@headlessui/react"
 import { Fragment } from "react"
 import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
+import { useNavigate } from "react-router-dom"
 
 type TaskCardProps = {
     task: Task
 }
 
 export default function TaskCard({task} : TaskCardProps) {
+
+    const navigate = useNavigate()
+
   return (
     <li className="p-5 bg-white border border-slate-300 flex justify-between gap-3">
         <div className="min-w-0 flex flex-col gap-y-4">
@@ -23,10 +27,10 @@ export default function TaskCard({task} : TaskCardProps) {
 
         <div className="flex shrink-0  gap-x-6">
             <Menu as="div" className="relative flex-none">
-                <Menu.Button className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
+                <MenuButton className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
                     <span className="sr-only">opciones</span>
                     <EllipsisVerticalIcon className="h-9 w-9" aria-hidden="true" />
-                </Menu.Button>
+                </MenuButton>
                 <Transition as={Fragment} enter="transition ease-out duration-100" enterFrom="transform opacity-0 scale-95"
                     enterTo="transform opacity-100 scale-100" leave="transition ease-in duration-75"
                     leaveFrom="transform opacity-100 scale-100" leaveTo="transform opacity-0 scale-95">
@@ -38,7 +42,11 @@ export default function TaskCard({task} : TaskCardProps) {
                             </button>
                         </MenuItem>
                         <MenuItem>
-                            <button type='button' className='block px-3 py-1 text-sm leading-6 text-gray-900'>
+                            <button 
+                                type='button' 
+                                className='block px-3 py-1 text-sm leading-6 text-gray-900'
+                                onClick={() => navigate(location.pathname + `?editTask=${task._id}`)}
+                                >
                                 Editar Tarea
                             </button>
                         </MenuItem>
