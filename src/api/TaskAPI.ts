9373043d83a136c5a8823cd64c10a031bdc38a06
Milton Munoz <livemilton@gmail.com>
2026-02-22 08:33:22 -0500

    frontend-application :sparkles: Getting the task to edit

--- src/api/TaskAPI.ts
+++ src/api/TaskAPI.ts
@@ -1,10 +1,11 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import { type TaskFormData, type Project } from "../types";
+import { type TaskFormData, type Project, type Task } from "../types";
 
 type TaskAPI = {
     formData: TaskFormData
     projectId: Project['_id']
+    taskId: Task['_id']
 }
 
 export async function createTask({formData, projectId}:Pick<TaskAPI, 'formData' | 'projectId'>) {
@@ -18,3 +19,15 @@ export async function createTask({formData, projectId}:Pick<TaskAPI, 'formData' 
         }
     }
 }
+
+export async function getTaskById({projectId, taskId} : Pick<TaskAPI, 'projectId' | 'taskId'>) {
+    try {
+        const url = `/projects/${projectId}/tasks/${taskId}`
+        const { data } = await api(url)
+        return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response){
+            throw new Error(error.response.data.error)
+        }
+    }
+}   
--- src/components/tasks/EditTaskData.tsx
+++ src/components/tasks/EditTaskData.tsx
@@ -1,11 +1,24 @@
-import { useLocation } from "react-router-dom"
+import { getTaskById } from "@/api/TaskAPI"
+import { useQuery } from "@tanstack/react-query"
+import { useLocation, useParams } from "react-router-dom"
 
 export default function EditTaskData() {
 
+    const params = useParams()
+    console.log(params)
+    const projectId = params.projectId!
     const location = useLocation()
     const queryParams = new URLSearchParams(location.search)
-    const taskId = queryParams.get('editTask')
+    const taskId = queryParams.get('editTask')!
     console.log(taskId)
+
+    const { data } = useQuery({
+        queryKey: ['task', taskId],
+        queryFn:() => getTaskById({projectId, taskId})
+    })
+
+    console.log(data)
+
   return (
     <div>EditTaskData</div>
   )
