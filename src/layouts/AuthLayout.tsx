a7b87fe0bd55a483cd1b2b0f2df5640814bafb23
Milton Munoz <livemilton@gmail.com>
2026-02-25 21:02:23 -0500

    frontend-application :sparkles: Creating the function to register the Account

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,6 +1,7 @@
 import Logo from '@/components/Logo'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
 export default function AuthLayout() {
   return (
@@ -10,13 +11,18 @@ export default function AuthLayout() {
         <div className='bg-gray-800 min-h-screen'>
             <div className='py-10 lg:py-20 mx-auto w-[450px]'>
                 <Logo />
-                
+
                 <div className='mt-10'>
 
                     <Outlet />
                 </div>
             </div>
         </div>
+
+        <ToastContainer
+            pauseOnHover={false}
+            pauseOnFocusLoss={false}
+        />
         
 
     </>
