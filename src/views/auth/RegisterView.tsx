9e4f956483386eca334db932ebb59de36e586f32
Milton Munoz <livemilton@gmail.com>
2026-03-09 20:53:30 -0500

    frontend-application :sparkles: Creating the View to reset password

--- src/views/auth/RegisterView.tsx
+++ src/views/auth/RegisterView.tsx
@@ -146,6 +146,12 @@ export default function RegisterView() {
                   className="text-center text-gray-300 font-normal"
               >¿Ya tienes cuenta? Iniciar Sesión
               </Link>
+
+                <Link
+                  to={'/auth/forgot-password'}
+                  className="text-center text-gray-300 font-normal"
+              >¿Olvidaste tu contraseña? Reestablecer
+              </Link>
       
             </nav>
     </>
